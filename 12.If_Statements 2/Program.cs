6343609840c575703a8fadc1e7c74dad897a5574
Dell-User <Dell-User@DESKTOP-GAPN8T9>
2023-10-10 08:25:12 +0200

    Add project files.

---
+++ 12.If_Statements 2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _12.If_Statements_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMax(2, 10));
+            Console.ReadLine();
+
+            static int GetMax(int num1, int num2)
+            {
+                if (num1 > num2)
+                {
+                    return num1; ;
+                }
+                else { return num2; }
+            }
+        }
+    }
+}
